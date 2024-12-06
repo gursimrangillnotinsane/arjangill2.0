f930fb9dff440f8691e423c854a6d15b9d7f56b0
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-12-05 21:00:51 -0500

    servives section that i finally like

--- components/services.tsx
+++ components/services.tsx
@@ -40,19 +40,23 @@ const services = () => {
             })
             .to(".serviceright0", {
                 display: "flex",
-                left: '-75%',
+                left: '-100%',
+                top: '5%'
                 // duration: 1,
                 // ease: "power2.out",
             })
             .to(".serviceright1", {
-                left: '-150%',
+                left: '-200%',
                 display: "flex",
+                top: '10%'
                 // duration: 1,
                 // ease: "power2.out",
             })
             .to(".serviceright2", {
-                left: '-225%',
+                left: '-300%',
+
                 display: "flex",
+                top: '15%',
                 // duration: 1,
                 // ease: "power2.out",
             });
@@ -64,48 +68,48 @@ const services = () => {
 
     return (
         <section className='containerServices h-screen'>
-            <div className="w-full flex justify-center items-center pt-20 after:content-[''] after:w-32 after:bg-yellow-500 after:h-0.5 after:m-3" >
+            <div className="w-full flex justify-center items-center lg:pt-0 pt-20 after:content-[''] after:w-32 after:bg-yellow-500 after:h-0.5 after:m-3" >
                 <p>Services</p>
             </div>
             <div className="" data-aos="fade-up">
 
                 <div className="section-title ">
-                    <h2>Services</h2>
+                    <h2 style={{ color: 'white' }}>Services</h2>
                     <p>Check our Services</p>
                 </div>
 
                 <div className="grid grid-cols-12 gap-4 text-white pt-12 ">
 
-                    <div className='flex flex-col trans1 col-span-1 lg:col-start-3 col-start-2 serviceright3 servicediv'>
-                        <div className="icon-container"> <BsMusicNoteList color="black" style={{ fontSize: '28px' }} /></div>
-                        <div className=" bg-[#cecbc8]  h-[370px] " data-aos="zoom-in" data-aos-delay="100">
+                    <div className='flex  trans1 lg:col-start-5 col-start-2 serviceright3 servicediv '>
+                        <div className="icon-container "><BsMusicNoteList color="black" style={{ fontSize: '28px' }} /></div>
+                        <div className="  bg-[#e0dfde]  h-[370px] sertext w-[30vw] " data-aos="zoom-in" data-aos-delay="200">
                             <div className="flex flex-col justify-center items-center py-[80px]  px-[20px]">
-                                <h4><a href="https://www.youtube.com/@arjangillsounds9191/videos" target="_blank"> Live Sound</a> </h4>
-                                <p className="text-black" >We provide live sound on various different type of functions such as marriage, concert, jagrata and much more</p>
+                                <h4> <a href="#manu">Live Sound</a> </h4>
+                                <p className='text-black'>We provide live sound on various different type of functions such as marriage, concert, jagrata and much more</p>
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-col trans1 serviceright0 servicediv trans1'>
-                        <div className="icon-container"><FaRegHandshake color="black" style={{ fontSize: '28px' }} /></div>
-                        <div className="  bg-[#cecbc8]  h-[370px] " data-aos="zoom-in" data-aos-delay="200">
+                    <div className='flex  trans1 serviceright0 servicediv '>
+                        <div className="icon-container mt-[10%]"><FaRegHandshake color="black" style={{ fontSize: '28px' }} /></div>
+                        <div className="  bg-[#e0dfde]  h-[370px] sertext w-[30vw] " data-aos="zoom-in" data-aos-delay="200">
                             <div className="flex flex-col justify-center items-center py-[80px]  px-[20px]">
                                 <h4> <a href="#manu">Retail</a> </h4>
                                 <p className='text-black'>We sell various kind of sound as well as ligh products</p>
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-col trans1 serviceright1 servicediv'>
-                        <div className="icon-container"><IoHammerOutline color="black" style={{ fontSize: '28px' }} /></div>
-                        <div className="bg-[#cecbc8] h-[370px]" data-aos="zoom-in" data-aos-delay="300">
+                    <div className='flex  trans1 serviceright1 servicediv'>
+                        <div className="icon-container mt-[20%]"><IoHammerOutline color="black" style={{ fontSize: '28px' }} /></div>
+                        <div className="bg-[#e0dfde] h-[370px] sertext w-[30vw]" data-aos="zoom-in" data-aos-delay="300">
                             <div className="flex flex-col justify-center items-center py-[80px]  px-[20px]">
                                 <h4>Installation</h4>
                                 <p className='text-black'>We install sound systems including speaker, mixer, amplifier in various religious places, schools, bars ,hotel and more</p>
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-col trans1 serviceright2 servicediv'>
-                        <div className="icon-container"><BsBoxes color="black" style={{ fontSize: '28px' }} /></div>
-                        <div className=" bg-[#cecbc8] h-[370px]" data-aos="zoom-in" data-aos-delay="100">
+                    <div className='flex  trans1 serviceright2 servicediv'>
+                        <div className="icon-container mt-[30%]"><BsBoxes color="black" style={{ fontSize: '28px' }} /></div>
+                        <div className=" bg-[#e0dfde] h-[370px] sertext w-[30vw]" data-aos="zoom-in" data-aos-delay="100">
                             <div className="flex flex-col justify-center items-center py-[80px]  px-[20px]">
                                 <h4><a href="#manu">Manufactuting</a> </h4>
                                 <p className='text-black'>Custom box, moniter, linear, bass with speakers and flight case for the same and amplifier and much more can be ordered </p>
